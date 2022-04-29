832088a97bb4053debfc23b9f248ae604d9652dd
Fortes <tokiya.kagami@gmail.com>
2022-04-29 09:49:55 +0800

    add eslint & tslint

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import eslintPlugin from 'vite-plugin-eslint'
 import * as path from "path"
 
 export default defineConfig({
@@ -14,6 +15,9 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    eslintPlugin({
+      cache: false
+    })
   ],
   base: process.env.NODE_ENV === "production" ? "/vue3_ec_backstage/" : "./",
   resolve: {
