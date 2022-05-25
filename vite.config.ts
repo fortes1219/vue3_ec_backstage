1f926af2e84b39f52275f3dcb2ed77bf03b3f94c
Fortes Huang <tokiya.kagami@gmail.com>
2022-05-25 13:47:10 +0800

    Update vite.config.ts
    Fixing bundle settings.

--- vite.config.ts
+++ vite.config.ts
@@ -83,15 +83,6 @@ export default({mode}: ConfigEnv) => defineConfig({
         entryFileNames: "js/[name].[hash].js",
         assetFileNames: (info) => {
           let type = info.name.split(".")[1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico|webp/i.test(type)) {
-            type = "img";
-          } else if (/ttf|eot|woff|woff2/i.test(type)) {
-            type = "fonts"
-          } else if (/mp4|webm|ogg/i.test(type)) {
-            type = "video"
-          } else if (/mp3|wav/i.test(type)) {
-            type = "sound"
-          }
           return `${type}/[name]-[hash].[ext]`;
         },
       }
